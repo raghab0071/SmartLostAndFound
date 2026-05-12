3f5efe05abd90ed0cb683efe0bd05b6498a48fd6
raghab0071 <msayantika70@gmail.com>
2026-05-12 22:12:37 +0530

    Export API_BASE constant from api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_REACT_APP_BACKEND_URL
+export const API_BASE = import.meta.env.VITE_REACT_APP_BACKEND_URL
 
 const api = axios.create({
   baseURL: `${API_BASE}/api`,
